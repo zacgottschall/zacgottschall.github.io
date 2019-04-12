3682a8e00619192984fafebb883598fa454a289b
Zac G <zachary.m.gottschall.20@dartmouth.edu>
2019-04-11 23:55:08 -0400

    release version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up');
+/* eslint-disable no-plusplus */
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  num++;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}, 1000);
